046fd745a0d10d689db629585c1472673085a141
liye <liye@gmail.com>
2022-11-11 18:06:58 +0800

    Push commit

---
+++ src/main/java/com/itheima/reggie/PushTest.java
@@ -0,0 +1,12 @@
+package com.itheima.reggie;
+
+/**
+ * @Author Li Ye
+ * @Version 1.0
+ */
+
+public class PushTest {
+    public static void main(String[] args) {
+        System.out.println("push test!");
+    }
+}
