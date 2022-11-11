e66c4fce690b212709e3d6c594a1168e70ac1485
DW1OR <108138790+DW1OR@users.noreply.github.com>
2022-11-11 18:21:00 +0800

    pull commit

--- src/main/java/com/itheima/reggie/PushTest.java
+++ src/main/java/com/itheima/reggie/PushTest.java
@@ -1,12 +1,7 @@
 package com.itheima.reggie;
 
-/**
- * @Author Li Ye
- * @Version 1.0
- */
-
 public class PushTest {
     public static void main(String[] args) {
-        System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
